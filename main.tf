6765e9bd489ae4f96cd6fbf8d0acd54b37ef25ae
Rohit <rohitmarathe7989@gmail.com>
2023-10-28 18:49:38 +0530

    made changes in the file

--- main.tf
+++ main.tf
@@ -1,58 +1,23 @@
 provider "aws" {
-  region = "ap-south-1"
+region = "us-west-1" # Replace with your desired region
 }
 
-# Create a security group in the default VPC
-resource "aws_security_group" "ubuntu_sg" {
-  name_prefix = "ubuntu-sg"
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Create an EC2 instance
+  resource "aws_instance" "example" {
+    count = 1 # Change the count to the desired number of instances
+    ami = "ami-019290fca71f9a776" # Replace with the desired AMI ID
+    instance_type = "t2.micro"
+    key_name = "my machine key pair"
+    tags = {
+    Name = "ubuntu-server-${count. index + 1}"
 }
 
-# Create an instance in the default subnet using the security group
-resource "aws_instance" "ubuntu" {
-  instance_type = "t2.micro"
-  ami           = data.aws_ami.ubuntu.id
-  key_name      = "devops-2-mumbai"
-  vpc_security_group_ids = [aws_security_group.ubuntu_sg.id]
-  tags = {
-    Name = "ubuntu-server"
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello, World!' > /home/ubuntu/hello.txt",
+    ]
   }
 }
-
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+  output "public_ip" {
+    value = aws_instance.example[*].public_ip
 }
-
-output "public_dns" {
-  value = aws_instance.ubuntu.public_dns
-}
-
